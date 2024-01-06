d9a40c742f532c230ac3de293f474a7a2b34d594
Max Zinovik <zinovik@gmail.com>
2024-01-06 17:53:18 +0100

    increase json limit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
         origin: ['https://zinovik.github.io'],
     });
+    app.use(json({ limit: '5mb' }));
     await app.listen(8080);
 }
 bootstrap();
