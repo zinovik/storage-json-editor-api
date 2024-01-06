26e76d912e725cf4332534e0968ae998ff50fa70
Max Zinovik <zinovik@gmail.com>
2024-01-06 17:57:43 +0100

    replace express import with body-parser import

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { json } from 'express';
+import { json } from 'body-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
