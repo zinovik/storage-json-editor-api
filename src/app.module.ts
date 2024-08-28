fbed636d438b6349f426a9a37c5b920580343d1f
Max Zinovik <zinovik@gmail.com>
2024-08-28 19:56:39 +0200

    remove gallery module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JsonModule } from './json/json.module';
-import { GalleryModule } from './gallery/gallery.module';
 
 @Module({
-    imports: [JsonModule, GalleryModule],
+    imports: [JsonModule],
 })
 export class AppModule {}
